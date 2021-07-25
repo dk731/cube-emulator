a4f2444d99a5b137257356d06c199e4b2c2947c8
dk731 <demid.kaidalov@gmail.com>
2021-07-25 22:59:35 +0300

    Working grid rendering

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const fs = require("fs");
 const app = express();
 const port = 3000;
@@ -7,10 +7,9 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/node_modules/three"));
 
 app.get("/", (req, res) => {
-    res.sendFile("/index.html");
+  res.sendFile("/index.html");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
--- public/index.js
+++ public/index.js
@@ -1,67 +1,120 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 var renderer = new THREE.WebGLRenderer();
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enablePan = false;
+controls.maxDistance = 100;
+controls.target = new THREE.Vector3(0.0, 0.0, 0.0);
+controls.minDistance = 5;
 
-camera.position.set(-100, 0, 0);
+camera.position.set(-35, 35, 35);
 controls.update();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-
 document.body.appendChild(renderer.domElement);
 
+const stats = Stats();
+document.body.appendChild(stats.dom);
+
 function windowResize() {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix()
-};
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
 
-window.addEventListener('resize', windowResize);
-
+window.addEventListener("resize", windowResize);
 
 /////////////////
-sphere_r = 0.3
-grid_dist = 2.5
+sphere_r = 0.2;
+grid_dist = 2.5;
 grid_size = new THREE.Vector3(16, 16, 16);
 
-mesh = new THREE.InstancedMesh(new THREE.SphereGeometry(sphere_r, 32, 32), new THREE.MeshBasicMaterial({ color: 0xffffff }), grid_size.x * grid_size.y * grid_size.z);
+mesh = new THREE.InstancedMesh(
+  new THREE.SphereGeometry(sphere_r, 32, 32),
+  new THREE.MeshBasicMaterial({ color: 0xffffff }),
+  grid_size.x * grid_size.y * grid_size.z
+);
 mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
 scene.add(mesh);
 
+// Setup colors array of grid size
+colors_array = Array.from({ length: grid_size.x }, () =>
+  Array.from({ length: grid_size.y }, () =>
+    Array.from({ length: grid_size.z }, () => new THREE.Color(0x010101))
+  )
+);
+
+// for (let z = 0; z < grid_size.z; z++)
+//   for (let y = 0; y < grid_size.y; y++)
+//     for (let x = 0; x < grid_size.x; x++)
+//       colors_array[x][y][z] = new THREE.Color(0xffffff);
+
 const dummy = new THREE.Object3D();
 
+setup_grid_mesh();
 animate();
 
 ////////////////
 
 function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
+  requestAnimationFrame(animate);
+  controls.update();
 
-    render();
+  render();
+  stats.update();
 }
 
-function render() {
-    console.log("rendering")
-    if (mesh) {
-        start_point = new THREE.Vector3();
-        start_point.copy(grid_size)
-        start_point.subScalar(1);
-        start_point.multiply(new THREE.Vector3(grid_dist, grid_dist, grid_dist));
-        start_point.divideScalar(2);
-        start_point.multiply(new THREE.Vector3(-1, 1, -1));
-        let i = 0;
-        for (let z = 0; z < grid_size.z; z++) {
-            for (let y = 0; y < grid_size.y; y++) {
-                for (let x = 0; x < grid_size.x; x++) {
-                    dummy.position.copy((new THREE.Vector3(x, y, z)).multiplyScalar(grid_dist).add(start_point));
-                    dummy.updateMatrix();
-                    mesh.setMatrixAt(i++, dummy.matrix);
-                }
-            }
-        }
-        mesh.instanceMatrix.needsUpdate = true;
+function setup_grid_mesh() {
+  start_point = new THREE.Vector3()
+    .copy(grid_size)
+    .subScalar(1)
+    .multiply(new THREE.Vector3(grid_dist, grid_dist, grid_dist))
+    .divideScalar(2)
+    .multiply(new THREE.Vector3(-1, 1, 1));
+
+  let i = 0;
+  for (let z = 0; z < grid_size.z; z++) {
+    for (let y = 0; y < grid_size.y; y++) {
+      for (let x = 0; x < grid_size.x; x++) {
+        dummy.position.copy(
+          new THREE.Vector3(x, -y, -z)
+            .multiplyScalar(grid_dist)
+            .add(start_point)
+        );
+        dummy.updateMatrix();
+        mesh.setMatrixAt(i, dummy.matrix);
+        mesh.setColorAt(i++, colors_array[x][y][z]);
+      }
     }
-    renderer.render(scene, camera);
+  }
+  mesh.instanceColor.needsUpdate = true;
+  mesh.instanceMatrix.needsUpdate = true;
+}
+
+function gen_rand_vec3() {}
+
+function render() {
+  update_colors();
+  colors_array[Math.floor(Math.random() * grid_size.x)][
+    Math.floor(Math.random() * grid_size.y)
+  ][Math.floor(Math.random() * grid_size.z)] = new THREE.Color(
+    0xffffff * Math.random()
+  );
+  renderer.render(scene, camera);
+}
+
+function update_colors() {
+  let i = 0;
+  for (let z = 0; z < grid_size.z; z++)
+    for (let y = 0; y < grid_size.y; y++)
+      for (let x = 0; x < grid_size.x; x++)
+        mesh.setColorAt(i++, colors_array[x][y][z]);
+
+  mesh.instanceColor.needsUpdate = true;
 }
